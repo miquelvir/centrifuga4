b7c71a9b919def36cae7f51fc4106331c3da8b09
miquelvir <vazquezrius.miquel@gmail.com>
2021-01-12 13:33:16 +0100

    wk

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ app = centrifuga4.init_app(HerokuProductionConfig)
 
 if __name__ == "__main__":
     with app.app_context():
-        app.run(host=app.config['BACKEND_SERVER_HOST'],
+        app.run(host=app.config['BACKEND_SERVER_HOST'],  # todo config vars directly
                 port=app.config['BACKEND_SERVER_PORT'],
                 ssl_context=(app.config['SSL_CERT'],
                              app.config['SSL_KEY']))
