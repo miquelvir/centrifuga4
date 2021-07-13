508fcd70bf19ef0cc0c6b57b3ceb03028cc184aa
miquelvir <vazquezrius.miquel@gmail.com>
2021-07-13 17:29:36 +0200

    manual db for empty db

--- development/manual_db_utils/generate_new_user_db_heroku.py
+++ development/manual_db_utils/generate_new_user_db_heroku.py
@@ -7,25 +7,38 @@ load_dotenv(join(dirname(__file__), "../../.env"))
 from server.models import User, Need
 
 
+def ask_admin_user():
+    admin = User(
+        id=User.generate_new_id(),
+        name=input("name: "),
+        surname1=input("surname1: "),
+        surname2=input("surname2: "),
+        email=input("email: "),
+        password_hash=User.hash_password(input("password: ")),
+    )
+
+    from generate_sample_db import all_needs
+
+    for need in all_needs:
+        admin.needs.append(Need.query.filter(Need.id == need.id).first())
+    return admin
+
+
+def add_admin_user():
+    from server import db
+
+    admin = ask_admin_user()
+    db.session.add(admin)
+
+
 if __name__ == "__main__":
     import server
-    from generate_sample_db import all_needs
+
     import config
 
     if input("type 'production'") == "production":
         app = server.init_app(config.HerokuManualLiveConfig)
         with app.app_context():
-            admin = User(
-                id=User.generate_new_id(),
-                name="admin4",
-                surname1="admin",
-                surname2="admin",
-                email="admin4@gmail.com",
-                password_hash=User.hash_password("admin4"),
-            )
 
-            for need in all_needs:
-                admin.needs.append(Need.query.filter(Need.id == need.id).first())
-
-            server.db.session.add(admin)
+            add_admin_user()
             server.db.session.commit()
