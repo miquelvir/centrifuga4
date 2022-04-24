21d9f392de179674cc1c609c87df492fab80275e
🧹 black <lint-action@samuelmeuli.com>
2022-04-24 09:19:42 +0000

    🧹 reformatted

--- server/services/totp_service.py
+++ server/services/totp_service.py
@@ -7,9 +7,7 @@ from flask import current_app
 
 class TotpService:
     @staticmethod
-    def generate_totp(
-            totp_secret
-    ) -> str:
+    def generate_totp(totp_secret) -> str:
         """ :returns the current TOTP """
         return pyotp.TOTP(totp_secret).now()
 
@@ -22,16 +20,30 @@ class TotpService:
 
     @staticmethod
     def _encryption_key(encryption_key: str = None):
-        return current_app.config["TOTP_ENCRYPTION_KEY"] if encryption_key is None else encryption_key
+        return (
+            current_app.config["TOTP_ENCRYPTION_KEY"]
+            if encryption_key is None
+            else encryption_key
+        )
 
     @staticmethod
     def encrypt_totp_secret(totp_secret, encryption_key: str = None) -> str:
-        return Fernet(bytes(TotpService._encryption_key(encryption_key), 'utf-8')).encrypt(bytes(totp_secret, 'utf-8')).decode('utf-8')
+        return (
+            Fernet(bytes(TotpService._encryption_key(encryption_key), "utf-8"))
+            .encrypt(bytes(totp_secret, "utf-8"))
+            .decode("utf-8")
+        )
 
     @staticmethod
     def decrypt_totp_secret(encrypted_secret, encryption_key: str = None) -> str:
-        return Fernet(bytes(TotpService._encryption_key(encryption_key), 'utf-8')).decrypt(bytes(encrypted_secret, 'utf-8')).decode('utf-8')
+        return (
+            Fernet(bytes(TotpService._encryption_key(encryption_key), "utf-8"))
+            .decrypt(bytes(encrypted_secret, "utf-8"))
+            .decode("utf-8")
+        )
 
     @staticmethod
     def generate_url(totp_secret: str, username: str) -> str:
-        return pyotp.totp.TOTP(totp_secret).provisioning_uri(name=username, issuer_name='centrífuga4 (Xamfrà)')
+        return pyotp.totp.TOTP(totp_secret).provisioning_uri(
+            name=username, issuer_name="centrífuga4 (Xamfrà)"
+        )
