4d87b6458b929fa72a9266bb968e4a7ea62b0b03
miquelvir <41950283+miquelvir@users.noreply.github.com>
2026-07-12 10:37:14 +0000

    Harden wkhtmltopdf discovery

--- server/pdfs/wkhtmltopdf.py
+++ server/pdfs/wkhtmltopdf.py
@@ -5,5 +5,21 @@ import pdfkit
 
 
 def get_config():
-    wkhtmltopdf_path = os.environ.get("wkhtmltopdf") or shutil.which("wkhtmltopdf")
-    return pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path)
+    env_path = os.environ.get("wkhtmltopdf")
+    if env_path:
+        return pdfkit.configuration(wkhtmltopdf=env_path)
+
+    for path in [
+        "/usr/bin/wkhtmltopdf",
+        "/app/bin/wkhtmltopdf",
+        "/app/.apt/usr/bin/wkhtmltopdf",
+        "/usr/local/bin/wkhtmltopdf",
+    ]:
+        if os.path.exists(path):
+            return pdfkit.configuration(wkhtmltopdf=path)
+
+    which_path = shutil.which("wkhtmltopdf")
+    if which_path:
+        return pdfkit.configuration(wkhtmltopdf=which_path)
+
+    return pdfkit.configuration(wkhtmltopdf=None)
